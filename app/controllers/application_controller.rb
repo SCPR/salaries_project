cd91adb7376bc211344139b126da4ca32acc35aa
Bryan Ricker <bricker88@gmail.com>
2014-02-11 12:50:49 -0800

    Remove trailing whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   def home
     @departments = Department.order("dept_total desc")
     @top_jobs    = Employee.order("salary desc").first(25)
   end
-  
+
   def list
     if params[:slug]
       @department = Department.where(slug: params[:slug]).first!
@@ -13,11 +13,11 @@ class ApplicationController < ActionController::Base
     else
       @employees = Employee.scoped
     end
-    
+
     if params[:query]
       @employees = @employees.where("job_title like ?", "%#{params[:query]}%")
     end
-    
+
     @employees = @employees.order("salary desc").page(params[:page]).per(25)
   end
 end
