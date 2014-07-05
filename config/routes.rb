87b9af013aec821d0b10c642c737840f65bec722
Bryan Ricker <bricker88@gmail.com>
2014-07-04 22:02:28 -0700

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SalariesProject::Application.routes.draw do
   match "/list/:slug" => "application#list", as: :department
   match "/list"       => "application#list", as: :list
-  
+
   root to: "application#home"
 end
