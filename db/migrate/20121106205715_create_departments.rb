d981c66490cc4eca9115a4ba4d11fcae7bdcd9ab
Bryan Ricker <bricker88@gmail.com>
2014-07-04 22:01:52 -0700

    Remove some unused files

--- db/migrate/20121106205715_create_departments.rb
+++
@@ -1,9 +0,0 @@
-class CreateDepartments < ActiveRecord::Migration
-  def change
-    create_table :departments do |t|
-      t.string :slug
-      t.string :name
-      t.decimal :dept_total
-    end
-  end
-end
